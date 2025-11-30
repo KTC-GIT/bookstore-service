b5068ae64757dab5f339d04d2334ce1396bc9a2d
ktc <ktc1205@gmail.com>
2025-11-30 20:08:00 +0900

    FIX: RabbitMQ Config 설정추가

--- src/main/java/com/practice/bookstore/global/config/RabbitMqConfig.java
+++ src/main/java/com/practice/bookstore/global/config/RabbitMqConfig.java
@@ -1,6 +1,10 @@
 package com.practice.bookstore.global.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -13,5 +17,25 @@ public class RabbitMqConfig {
     @Bean
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    // 큐 생성 (durable=true: 서버 재시작해도 유지됨)
+    @Bean
+    public Queue bookstoreQueue() {
+        return new Queue("bookstore.queue",true);
+    }
+
+    // 익스체인지 생성
+    @Bean
+    public DirectExchange bookstoreExchange() {
+        return new DirectExchange("bookstore.exchange");
+    }
+
+    // 바인딩 (큐와 익스체인지를 'mail.key'로 연결
+    @Bean
+    public Binding binding(Queue bookstoreQueue, DirectExchange bookstoreExchange) {
+        return BindingBuilder.bind(bookstoreQueue)
+                .to(bookstoreExchange)
+                .with("mail.key");
     }
 }
